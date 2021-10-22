a36582eb398e886c9968a1ad2c759d48ea644cca
Iryna Kresinska <kresi.ira@gmail.com>
2021-10-22 21:18:17 +0200

    finn.no recomendation section
    part2.1: style single recommendation item with css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
+import DotsIcon from './svgIcons/dots.svg';
+import HeartIcon from './svgIcons/heart.svg';
 
 const FinnRecommendations = () => {
     return (
@@ -12,7 +15,7 @@ const FinnRecommendations = () => {
             <Recommendation/>
         </section>
     )
-}
+};
 
 const Recommendation = () => {
     return (
@@ -21,25 +24,32 @@ const Recommendation = () => {
             <Description/>
         </article>
     )
-}
+};
 
 const Image = () => {
-    return <img src={'https://images.finncdn.no/dynamic/480w/logo/result/-3002/iad_1359802226105336855finn-logo-hovedlogo-rgb-ikketransp.png'} alt={'finn logo'}/>
-}
+    return (
+        <div className={"img-wrapper"}>
+            <img src={HeartIcon} className={"heart"}/>
+            <img
+                className={"main-img"}
+                src={'https://images.finncdn.no/dynamic/480w/logo/result/-3002/iad_1359802226105336855finn-logo-hovedlogo-rgb-ikketransp.png'}
+                alt={'finn logo'}
+            />
+        </div>
+    );
+};
 
 const Description = () => {
     return (
         <>
-            <div>
+            <div className={"description"}>
                 Oslo
-                <button>&#8230;</button>
+                <img src={DotsIcon} className={"icon"}/>
             </div>
             <h3>FINN.no is looking for Software Developers</h3>
-            <div>
-                FIND
-            </div>
+            <div className={"description"}>FIND</div>
         </>
-    )
-}
+    );
+};
 
 ReactDom.render(<FinnRecommendations/>, document.getElementById('root'));

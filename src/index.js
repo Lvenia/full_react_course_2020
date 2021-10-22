ada1f551e04997db525f85ae4983a81fcf31d98f
Iryna Kresinska <kresi.ira@gmail.com>
2021-10-22 13:35:18 +0200

    React.createElement(type, props, children)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const FirstComponent = () => {
-    return (
-        <>
-            <h1>Hello World!</h1>
-            <p>This is the fist component.</p>
-        </>
-    )
-};
+// const FirstComponent = () => {
+//     return (
+//         <>
+//             <h1>Hello World!</h1>
+//             <p>This is the fist component.</p>
+//         </>
+//     )
+// };
 
-ReactDom.render(<FirstComponent/>, document.getElementById('root'));
+const SecondComponent = () => {
+    return React.createElement('h1', null, 'Hello World!');
+}
+
+ReactDom.render(<SecondComponent/>, document.getElementById('root'));
